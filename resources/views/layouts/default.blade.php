4261bad2f5863722ebee2ff5a7cfa990095e28ef
Shiv-kumar-WebDev <shivkumarkain500063@gmail.com>
2024-10-08 18:14:56 +0530

    username added to dashboard

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -436,7 +436,7 @@
 							<img src="{{ url('public') }}/assets/images/demo/users/face11.jpg" class="w-32px h-32px rounded-pill" alt="">
 							<span class="status-indicator bg-success"></span>
 						</div>
-						<span class="d-none d-lg-inline-block mx-lg-2">Victoria</span>
+						<span class="d-none d-lg-inline-block mx-lg-2">{{ Auth::user()->name }}</span>
 					</a>
 
 					<div class="dropdown-menu dropdown-menu-end">
